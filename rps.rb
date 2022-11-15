4343a62ca507d06b0be1f6c0a5b355a9f9931100
musashi-634 <kazuaki.miyamoto.job@gmail.com>
2022-11-15 08:24:24 +0000

    【追加】あっち向いてホイの機能を実装

--- rps.rb
+++ rps.rb
@@ -3,49 +3,40 @@ class Rock_paper_scissors
     "1": "グー",
     "2": "チョキ",
     "3": "パー",
-    "0": "戦わない"
+    "q": "戦わない"
+  }
+  @@look_this_way_choice_hash = {
+    "1": "上",
+    "2": "下",
+    "3": "左",
+    "4": "右"
   }
   
   def initialize
-    @player_choices = ["", ""]
-    @cpu_choices = ["1", ""]
-    
     play_rock_paper_scissors
   end
   
   def play_rock_paper_scissors
-    win_rps_player = nil # プレイヤーのじゃんけんの勝敗を表すbool値
+    @player_choices = ["", ""]
+    @cpu_choices = ["1", "1"]
+    @win_rps_player = nil # プレイヤーのじゃんけんの勝敗を表すbool値
     
     puts "「じゃんけん...」"
-    puts "選択-> "\
-         "1（#{@@rps_choice_hash[:"1"]}） "\
-         "2（#{@@rps_choice_hash[:"2"]}） "\
-         "3（#{@@rps_choice_hash[:"3"]}） "\
-         "0（#{@@rps_choice_hash[:"0"]}）"
-    @player_choices[0] = gets.chomp
-    if @player_choices[0] == @@rps_choice_hash.key("戦わない").to_s
-      puts "対戦を終了します"
-      exit
-    end
-    puts "「ホイ！」"
-    show_choice
+    show_selectable_choice(@@rps_choice_hash)
+    get_choice("rock_paper_scissors")
+    puts "「ポン！」"
+    show_decided_choice
+    
+    # あいこの場合の処理
     if @player_choices[0] == @cpu_choices[0]
       is_even = true  # あいこの判定
       while is_even
         puts "「あいこで...」"
-        puts "選択-> "\
-             "1（#{@@rps_choice_hash[:"1"]}） "\
-             "2（#{@@rps_choice_hash[:"2"]}） "\
-             "3（#{@@rps_choice_hash[:"3"]}） "\
-             "0（#{@@rps_choice_hash[:"0"]}）"
-        @player_choices[0] = gets.chomp
-        if @player_choices[0] == @@rps_choice_hash.key("戦わない").to_s
-          puts "対戦を終了します"
-          exit
-        end
+        show_selectable_choice(@@rps_choice_hash)
+        get_choice("rock_paper_scissors")
         puts "「ショ！」"
-        show_choice
-        if @player_choices[0] != @cpu_choices[0]
+        show_decided_choice
+        if @player_choices[0] != @cpu_choices[0]  # あいこでない場合
           is_even = false
         end
       end
@@ -55,47 +46,94 @@ class Rock_paper_scissors
     case @player_choices[0]
     when @@rps_choice_hash.key("グー").to_s
       if @cpu_choices[0] == @@rps_choice_hash.key("チョキ").to_s
-        win_rps_player = true
+        @win_rps_player = true
       else
-        win_rps_player = false
+        @win_rps_player = false
       end
     when @@rps_choice_hash.key("チョキ").to_s
       if @cpu_choices[0] == @@rps_choice_hash.key("パー").to_s
-        win_rps_player = true
+        @win_rps_player = true
       else
-        win_rps_player = false
+        @win_rps_player = false
       end
     when @@rps_choice_hash.key("パー").to_s
       if @cpu_choices[0] == @@rps_choice_hash.key("グー").to_s
-        win_rps_player = true
+        @win_rps_player = true
       else
-        win_rps_player = false
+        @win_rps_player = false
       end
-    end
-    
-    if win_rps_player
-      puts "じゃんけんに勝利しました"
-    elsif win_rps_player == false
-      puts "じゃんけんに敗北しました"
     else
       puts "じゃんけんの勝敗が不明です"
+      exit
     end
-    # play_look_this_way
-  end
-  
-  def show_choice
-    if @player_choices[1] == ""
-      puts "----------------------------------"
-      puts "あなた：#{@@rps_choice_hash[@player_choices[0].to_sym]}を出しました"
-      puts "相手：#{@@rps_choice_hash[@cpu_choices[0].to_sym]}を出しました"
-      puts "----------------------------------"
-    else
-      
-    end
+    
+    play_look_this_way
   end
   
   def play_look_this_way
     puts "あっち向いて〜"
+    show_selectable_choice(@@look_this_way_choice_hash)
+    get_choice("look_this_way")
+    puts "「ホイ！」"
+    show_decided_choice
+    
+    # あっち向いてホイの勝敗の判定
+    if @player_choices[1] == @cpu_choices[1]
+      case @win_rps_player
+      when true
+        puts "あなたの勝ちです"
+      else
+        puts "あなたの負けです"
+      end
+    else
+      play_rock_paper_scissors
+    end
+  end
+  
+  def show_selectable_choice(select_hash)
+    output_string = "選択-> "
+    select_hash.each{|key, value|
+      output_string += "#{key.to_s}（#{value}） "
+    }
+    puts output_string
+  end
+  
+  def get_choice(game_name)
+    case game_name
+    when "rock_paper_scissors"
+      @player_choices[0] = gets.chomp
+      if @@rps_choice_hash.key?(@player_choices[0].to_sym) == false # 入力不正チェック
+        puts "入力が間違っています"
+        exit
+      end
+      if @player_choices[0] == @@rps_choice_hash.key("戦わない").to_s # やめる場合
+        puts "対戦を終了します"
+        exit
+      end
+    when "look_this_way"
+      @player_choices[1] = gets.chomp
+      if @@look_this_way_choice_hash.key?(@player_choices[1].to_sym) == false # 入力不正チェック
+        puts "入力が間違っています"
+        exit
+      end
+    else
+      puts "引数のゲーム名が間違っています"
+      exit
+    end
+  end
+  
+  def show_decided_choice
+    if @player_choices[1] == ""
+      puts "----------------------------------"
+      puts "あなた：#{@@rps_choice_hash[@player_choices[0].to_sym]}"
+      puts "相手：#{@@rps_choice_hash[@cpu_choices[0].to_sym]}"
+      puts "----------------------------------"
+    else
+      puts "----------------------------------"
+      puts "あなた：#{@@look_this_way_choice_hash[@player_choices[1].to_sym]}"
+      puts "相手：#{@@look_this_way_choice_hash[@cpu_choices[1].to_sym]}"
+      puts "----------------------------------"
+    end
   end
 end
 
